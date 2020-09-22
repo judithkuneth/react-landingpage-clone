44f318db8938d65e589049575eb4c2a42d98e94e
Judith Kuneth <judith.kuneth@gmail.com>
2020-09-22 19:19:22 +0200

    Add CSS Stylings

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import facebook from './images/facebook.svg';
 import linkedin from './images/linkedin.svg';
 import twitter from './images/twitter.svg';
 import calendarMobile from './images/calendarMobile.png';
+import triangle from './images/triangle.svg';
+import triangleOrange from './images/triangleOrange.svg';
 
 const logoStyles = css`
   height: 25px;
@@ -31,17 +33,19 @@ const calendarMobileStyles = css`
 `;
 
 const headerStyles = css`
-  background-color: #444655;
+  background-color: #fff;
   display: flex;
   justify-content: space-between;
-  padding: 15px 0;
+  padding: 10px 190px;
+  position: fixed;
+  top: 0px;
+  width: 100%;
 `;
 
 const navStyles = css`
-  /* display: center; */
   a {
     margin-right: 20px;
-    color: white;
+    color: #4a4a4a;
     text-decoration: none;
   }
 `;
@@ -52,48 +56,167 @@ const flagStyles = css`
 `;
 
 const buttonSignup = css`
-  border-radius: 12px;
-  border: 2px solid #ffffff;
-  height: 25px;
+  border-radius: 16px;
+  border: 2px solid #ff6384;
   background-color: transparent;
-  color: white;
+  color: #ff6384;
+  padding: 5px 8px;
+  margin: 0px 0px;
 `;
 
+const buttonGetStarted = css`
+  padding: 10px 20px;
+  margin: 10px 0px;
+`;
+
+const watchVideoButton = css`
+  background: #ffffff;
+  color: #009cbd;
+  text-decoration: underline;
+  padding: 10px 0px;
+  font-size: 20px;
+  margin-top: 5px;
+  margin-left: 0px;
+  padding: 0px 0px;
+`;
+const generalSectionStyle = css`
+  margin: 60px 190px;
+`;
 const firstSectionStyle = css`
+  ${generalSectionStyle}
+  h1 {
+    color: #757575;
+  }
   > div {
     display: flex;
     align-items: center;
   }
   img {
-    width: 55%;
+    width: 50%;
+    margin-top: 10px;
   }
 `;
 
 const secondSectionStyle = css`
   text-align: center;
+  button {
+    border-radius: 18px;
+    margin-bottom: 20px;
+  }
+  background-image: url(${triangle});
+  background-repeat: no-repeat;
+  background-size: 19%;
+  padding-bottom: 100px;
+`;
+
+const linkBold = css`
+  font-weight: bold;
+  font-size: 18px;
 `;
 
 const thirdSectionStyle = css`
+  ${generalSectionStyle}
   text-align: center;
   div {
     display: flex;
     align-items: center;
-  }
-  a {
-    text-decoration: none;
+    flex-wrap: wrap;
+    h1 {
+      color: black;
+    }
+    p {
+      color: black;
+    }
+    a {
+      text-decoration: none;
+      border: 2px solid #9896a4;
+      border-radius: 20%;
+      box-shadow: 0px 2px 0px 0px #000000;
+      height: 440px;
+      margin: 30px 20px 60px 10px;
+      padding: 20px 20px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 20%;
+      p {
+        color: #9896a4;
+        font-size: 16px;
+      }
+      @media (max-width: 700px) {
+        width: 50%;
+      }
+      @media (max-width: 460px) {
+        width: 100%;
+      }
+    }
   }
 `;
 
 const fourthSectionStyle = css`
   text-align: center;
+  background-image: url(${triangleOrange});
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: 14%;
+`;
+
+const imageRadarStyle = css`
+  padding-top: 40px;
+  padding-bottom: 40px;
 `;
 
 const fifthSectionStyles = css`
   text-align: center;
-  /* div {
+  /* background-repeat: no-repeat;
+  background-size: 25%; */
+  > div {
     display: flex;
     align-items: center;
-  } */
+    justify-content: center;
+    background-image: linear-gradient(139deg, #3023ae, #c86dd7);
+  }
+`;
+
+const sixthSectionStyles = css`
+  ${generalSectionStyle}
+  text-align: center;
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  a {
+    text-decoration: none;
+  }
+  img {
+    margin-left: 10px;
+  }
+`;
+
+const footerStyles = css`
+  ${generalSectionStyle}
+  text-align: left;
+  justify-content: center;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  a {
+    text-decoration: none;
+    margin-left: 10px;
+  }
+  p {
+    font-size: 16px;
+    color: #272727;
+    margin-bottom: 0px;
+  }
+`;
+
+const logoFooterStyles = css`
+  height: 28px;
 `;
 
 function App() {
@@ -121,8 +244,9 @@ function App() {
                 effective team collaboration and putting into action that impact
               </p>
               <input placeholder="Enter your mail" type="email"></input>
-              <button>Get Started - Free Forever</button>
-              <button>Watch Video/button</button>
+              <button css={buttonGetStarted}>Get Started - Free Forever</button>
+              <br />
+              <button css={watchVideoButton}>Watch Video</button>
             </div>
             <img src={banner} alt="" />
           </div>
@@ -139,6 +263,7 @@ function App() {
           </div>
           <img src={board} alt="" />
         </section>
+
         <section css={thirdSectionStyle}>
           <h1>Make Your Life Easier</h1>
           <p>Get started fast with suitable templates for every team.</p>
@@ -146,7 +271,7 @@ function App() {
           <div>
             <a href="#a">
               <img src={talkShow} alt="" />
-              <h1>Talk show- Event management</h1>
+              <h2>Talk show- Event management</h2>
               <p>
                 The Marketing department is responsible for running events.
                 Particularly, They mainly organize the seminar and talk show to
@@ -156,7 +281,7 @@ function App() {
 
             <a href="#a">
               <img src={eventMarketing} alt="" />
-              <h1>Event Marketing</h1>
+              <h2>Event Marketing</h2>
               <p>
                 Event Marketing has always been considered as an opportunity to
                 build relationships, earn the trust of partners or customers and
@@ -166,7 +291,7 @@ function App() {
 
             <a href="#a">
               <img src={hr} alt="" />
-              <h1>Human Resource</h1>
+              <h2>Human Resource</h2>
               <p>
                 The Human Resource Department has always faced the pressure to
                 do recruitment. For the large scale of the company, the labor
@@ -176,7 +301,7 @@ function App() {
 
             <a href="#a">
               <img src={bugTracker} alt="" />
-              <h1>Bug & Issue Tracker</h1>
+              <h2>Bug & Issue Tracker</h2>
               <p>
                 When It comes to the IT industry, Bug and Issue are inevitable
                 in every project. The team of IT need to determine the solution
@@ -185,43 +310,66 @@ function App() {
             </a>
           </div>
 
-          <a href="#a">View all case studies</a>
+          <a css={linkBold} href="#a">
+            View all case studies
+          </a>
         </section>
+
         <section css={fourthSectionStyle}>
           <h1>Single Place</h1>
           <p>Your love Apps can be integrated into UpDiagram.com</p>
-          <img src={radar} alt="" />
+          <img css={imageRadarStyle} src={radar} alt="" />
         </section>
+
         <section css={fifthSectionStyles}>
           <div>
-            <h1>Let inspire your team to take action</h1>
             <img css={logoSmallStyles} src={logoSmall} alt="" />
-            <input placeholder="Enter your mail" styles="mail"></input>
-            <button>Get Started - Free Forever</button>
+            <div>
+              <h1>Let inspire your team to take action</h1>
+              <input placeholder="Enter your mail" styles="mail"></input>
+              <button>Get Started - Free Forever</button>
+            </div>
+            <img css={calendarMobileStyles} src={calendarMobile} alt="" />
           </div>
-          <img css={calendarMobileStyles} src={calendarMobile} alt="" />
         </section>
       </main>
       <footer>
-        <section>
-          <h1>Products</h1>
-          <a href="#a">Templates</a>
-          <a href="#a">Pricing</a>
-          <h1>Company</h1>
-          <a href="#a">About</a>
-          <a href="#a">Contact Us</a>
-          <h1>Resources</h1>
-          <a href="#a">Blog</a>
-          <a href="#a">Testimonials</a>
-          <h1>Social networks</h1>
-          <img src={facebook} alt="" />
-          <img src={linkedin} alt="" />
-          <img src={twitter} alt="" />
+        <section css={sixthSectionStyles}>
+          <div>
+            <div>
+              <h4>Products</h4>
+              <a href="#a">Templates</a>
+              <br />
+              <a href="#a">Pricing</a>
+            </div>
+            <div>
+              <h4>Company</h4>
+              <a href="#a">About</a>
+              <br />
+              <a href="#a">Contact Us</a>
+            </div>
+            <div>
+              <h4>Resources</h4>
+              <a href="#a">Blog</a>
+              <br />
+              <a href="#a">Testimonials</a>
+            </div>
+            <div>
+              <h4>Social networks</h4>
+              <img src={facebook} alt="" />
+              <img src={linkedin} alt="" />
+              <img src={twitter} alt="" />
+            </div>
+          </div>
         </section>
-        <section>
-          <img src={logo} alt="" />
-          <p>Copyrights © 2019 UpDiagram. All rights reserved.</p>
-          <a href="#a">Terms of Policy and Service</a>
+        <section css={footerStyles}>
+          <div>
+            <img css={logoFooterStyles} src={logo} alt="" />
+            <div>
+              <p>Copyrights © 2019 UpDiagram. All rights reserved.</p>
+              <a href="#a">Terms of Policy and Service</a>
+            </div>
+          </div>
         </section>
       </footer>
     </>
--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ ReactDOM.render(
     <Global
       styles={css`
         /* Use better layout model (border-box) */
-        /* *,
+        *,
         *::before,
         *::after {
-          box-sizing: border-box; */
+          box-sizing: border-box;
         }
 
         body {
-        }
-        button {
-        }
-        h1 {
-        }
-        p {
+          font-family: sans-serif;
+
+          nav {
+            font-size: 13px;
+          }
+          h1 {
+            color: #262626;
+            font-size: 36px;
+            margin: 0px 0px;
+          }
+
+          h2 {
+            color: #3e5469;
+            font-size: 24px;
+          }
+          h4 {
+            color: #272727;
+            font-size: 18px;
+          }
+          p {
+            color: #9896a4;
+            margin: 10px;
+            line-height: 25px;
+            font-size: 18px;
+            font-weight: lighter;
+          }
+          a {
+            color: #009cbd;
+          }
+          input {
+            background: rgba(214, 216, 216, 0.64);
+            color: #9b9b9b;
+            border-radius: 6px;
+            border: #ffffff;
+            padding: 10px 20px;
+            margin-right: 10px;
+          }
+          button {
+            background: #009cbd;
+            color: #ffffff;
+            border-radius: 6px;
+            border: #ffffff;
+            padding: 10px 20px;
+            margin-right: 20px;
+            margin-top: 20px;
+          }
+          h1 {
+          }
+          p {
+          }
         }
       `}
     />
